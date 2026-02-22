044444af2b89c6aea1c95d2a1a192dfb24486287
Mohit Verma <mohit51341862@gmail.com>
2026-02-23 03:28:31 +0530

    setting the app context

--- frontend/src/context/AppContext.tsx
+++ frontend/src/context/AppContext.tsx
@@ -6,11 +6,11 @@ import toast, { Toaster } from "react-hot-toast";
 import Cookies from "js-cookie";
 import axios from "axios";
 
-export const utils_service = "http://35.154.186.96:5001";
-export const auth_service = "http://35.154.186.96:5000";
-export const user_service = "http://35.154.186.96:5002";
-export const job_service = "http://35.154.186.96:5003";
-export const payment_service = "http://35.154.186.96:5004";
+export const utils_service = "http://localhost:5001";
+export const auth_service = "http://localhost:5000";
+export const user_service = "http://localhost:5002";
+export const job_service = "http://localhost:5003";
+export const payment_service = "http://localhost:5004";
 
 const AppContext = createContext<AppContextType | undefined>(undefined);
 
